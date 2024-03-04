2db5c4a09c9e2aa810e351503eefdd19187c62e5
Thrila <Ifeanyindubizu503@gmail.com>
2024-03-05 00:47:05 +0100

    feat: switched hashmaps for switch statements

--- src/lib.rs
+++ src/lib.rs
@@ -1,99 +1,134 @@
-use std::collections::HashMap;
-
-pub fn convert_to_bin(text: &str) -> Vec<String> {
-    let text = text.to_string();
-    let mut ascii_string: Vec<String> = vec![];
-    for character in text.clone().into_bytes() {
-        let bin_to_num = format!("0{:b}", character);
-        ascii_string.push(bin_to_num);
+pub fn convert_to_bin(string: String) -> Vec<u8> {
+    let mut binary_vec = vec![];
+    for character in string.clone().into_bytes() {
+        binary_vec.push(character)
     }
-    println!("{:?}", ascii_string);
-    ascii_string
-}
-pub fn encode(bin_vec: Vec<String>) -> String {
-    let mut result = "".to_string();
-    let encode_map: HashMap<String, String> = HashMap::from([
-        ("01000001".to_string(), "N".to_string()),
-        ("01000010".to_string(), "O".to_string()),
-        ("01000011".to_string(), "P".to_string()),
-        ("01000100".to_string(), "Q".to_string()),
-        ("01000101".to_string(), "R".to_string()),
-        ("01000110".to_string(), "S".to_string()),
-        ("01000111".to_string(), "T".to_string()),
-        ("01001000".to_string(), "U".to_string()),
-        ("01001001".to_string(), "V".to_string()),
-        ("01001010".to_string(), "W".to_string()),
-        ("01001011".to_string(), "X".to_string()),
-        ("01001100".to_string(), "Y".to_string()),
-        ("01001101".to_string(), "Z".to_string()),
-        ("01001110".to_string(), "A".to_string()),
-        ("01001111".to_string(), "B".to_string()),
-        ("01010000".to_string(), "C".to_string()),
-        ("01010001".to_string(), "D".to_string()),
-        ("01010010".to_string(), "E".to_string()),
-        ("01010011".to_string(), "F".to_string()),
-        ("01010100".to_string(), "I".to_string()),
-        ("01010101".to_string(), "J".to_string()),
-        ("01010110".to_string(), "K".to_string()),
-        ("01010111".to_string(), "L".to_string()),
-        ("01011000".to_string(), "M".to_string()),
-        ("01011001".to_string(), "O".to_string()),
-        ("01011010".to_string(), "P".to_string()),
-        ("0100000".to_string(), " ".to_string()),
-    ]);
-    for bin in bin_vec {
-        match encode_map.get(&bin) {
-            Some(binary) => result += binary,
-            _ => println!("not found"),
-        }
-    }
-    result
+    binary_vec
 }
 
-pub fn string_vec(text: String) -> Vec<String> {
-    let mut char_vec = vec![];
-    for c in text.chars() {
-        char_vec.push(String::from(c))
+pub fn encoded(binary_vec: Vec<u8>) -> String {
+    let mut string = String::new();
+    for ascii in binary_vec {
+        let a = match ascii {
+            32 => "*",
+            65 => "N",
+            66 => "O",
+            67 => "P",
+            68 => "Q",
+            69 => "R",
+            70 => "S",
+            71 => "T",
+            72 => "U",
+            73 => "V",
+            74 => "W",
+            75 => "X",
+            76 => "Y",
+            77 => "Z",
+            78 => "A",
+            79 => "B",
+            80 => "C",
+            81 => "D",
+            82 => "E",
+            83 => "F",
+            84 => "G",
+            85 => "H",
+            86 => "I",
+            87 => "J",
+            88 => "K",
+            89 => "K",
+            90 => "L",
+            97 => "n",
+            98 => "o",
+            99 => "p",
+            100 => "q",
+            101 => "r",
+            102 => "s",
+            103 => "t",
+            104 => "u",
+            105 => "v",
+            106 => "w",
+            107 => "x",
+            108 => "y",
+            109 => "z",
+            110 => "a",
+            111 => "b",
+            112 => "c",
+            113 => "d",
+            114 => "e",
+            115 => "f",
+            116 => "g",
+            117 => "h",
+            118 => "i",
+            119 => "j",
+            120 => "k",
+            121 => "l",
+            122 => "m",
+            _ => "Only chars fro A-z allowed",
+        };
+        string += a;
     }
-    char_vec
+    string
 }
-
-pub fn decode(bin_vec: Vec<String>) -> String {
-    let mut result = String::new();
-    let decode_map: HashMap<String, String> = HashMap::from([
-        ("N".to_string(), "A".to_string()),
-        ("O".to_string(), "B".to_string()),
-        ("P".to_string(), "C".to_string()),
-        ("Q".to_string(), "D".to_string()),
-        ("R".to_string(), "E".to_string()),
-        ("S".to_string(), "F".to_string()),
-        ("T".to_string(), "G".to_string()),
-        ("U".to_string(), "H".to_string()),
-        ("V".to_string(), "I".to_string()),
-        ("W".to_string(), "J".to_string()),
-        ("X".to_string(), "K".to_string()),
-        ("Y".to_string(), "L".to_string()),
-        ("Z".to_string(), "M".to_string()),
-        ("A".to_string(), "N".to_string()),
-        ("B".to_string(), "O".to_string()),
-        ("C".to_string(), "P".to_string()),
-        ("D".to_string(), "Q".to_string()),
-        ("E".to_string(), "R".to_string()),
-        ("F".to_string(), "S".to_string()),
-        ("G".to_string(), "T".to_string()),
-        ("H".to_string(), "U".to_string()),
-        ("I".to_string(), "V".to_string()),
-        ("J".to_string(), "W".to_string()),
-        ("K".to_string(), "X".to_string()),
-        ("L".to_string(), "Y".to_string()),
-        ("M".to_string(), "Z".to_string()),
-        (" ".to_string(), " ".to_string()),
-    ]);
-    for bin in bin_vec {
-        match decode_map.get(&bin) {
-            Some(item) => result += item,
-            _ => println!("wrong item"),
-        }
+pub fn decode(binary_vec: Vec<u8>) -> String {
+    let mut string = String::new();
+    for ascii in binary_vec {
+        let a = match ascii {
+            42 => " ",
+            65 => "N",
+            66 => "O",
+            67 => "P",
+            68 => "Q",
+            69 => "R",
+            70 => "S",
+            71 => "T",
+            72 => "U",
+            73 => "V",
+            74 => "W",
+            75 => "X",
+            76 => "Y",
+            77 => "Z",
+            78 => "A",
+            79 => "B",
+            80 => "C",
+            81 => "D",
+            82 => "E",
+            83 => "F",
+            84 => "G",
+            85 => "H",
+            86 => "I",
+            87 => "J",
+            88 => "K",
+            89 => "L",
+            90 => "M",
+            97 => "n",
+            98 => "o",
+            99 => "p",
+            100 => "q",
+            101 => "r",
+            102 => "s",
+            103 => "t",
+            104 => "u",
+            105 => "v",
+            106 => "w",
+            107 => "x",
+            108 => "y",
+            109 => "z",
+            110 => "a",
+            111 => "b",
+            112 => "c",
+            113 => "d",
+            114 => "e",
+            115 => "f",
+            116 => "g",
+            117 => "h",
+            118 => "i",
+            119 => "j",
+            120 => "k",
+            121 => "l",
+            122 => "m",
+            _ => "Only chars fro A-z allowed",
+        };
+        string += a;
     }
-    result
+    string
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use rot::convert_to_bin;
 use rot::decode;
-use rot::encode;
-use rot::string_vec;
+use rot::encoded;
 
 fn main() {
-    let ans = convert_to_bin("HELLO MAN");
-    let ns = string_vec("URYYB ZNA".to_string());
-    let string = encode(ans);
-    let code = decode(ns);
-    println!("encoded {:?} decoded {:?}", string, code);
+    let str = convert_to_bin(String::from("The new world"));
+    let encod = encoded(str);
+    println!("{:?}", encod);
+    let scrambled = convert_to_bin(String::from("Gur*arj*jbeyq"));
+    let decoded = decode(scrambled);
+    println!("{:?}", decoded);
 }
