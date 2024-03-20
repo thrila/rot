d1e628e4d603dddca67d6fb119100fdc8e422971
Thrila <Ifeanyindubizu503@gmail.com>
2024-03-20 11:23:13 +0100

    add:clap
    added clap for command parsing

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,46 @@ use rot::convert_to_bin;
 use rot::decode;
 use rot::encoded;
 
+use clap::Parser;
+
 use std::fs;
 use std::fs::File;
 use std::io::Write;
+use std::path::PathBuf;
 
-fn get_arguments() -> Vec<String> {
-    let args: Vec<_> = std::env::args().collect(); // get all arguements passed to app
-    args
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct CliArgs {
+    target_path: PathBuf,
+    #[arg(short, long)]
+    option: String,
+    destination_path: String,
 }
 
 fn main() -> std::io::Result<()> {
-    let x = &get_arguments()[1];
+    let CliArgs {
+        target_path,
+        option,
+        destination_path,
+    } = CliArgs::parse();
 
-    match x.as_str() {
-        "-e" => {
-            let words = fs::read_to_string("foo.txt").expect("Should be able to read from file");
+    match option.as_str() {
+        "e" | "encode" => {
+            let words = fs::read_to_string(target_path).expect("Should be able to read from file");
             let encod = convert_to_bin(words);
             let jargon = encoded(encod);
-            let mut f = File::create("foobar.txt")?;
+            let mut f = File::create(destination_path)?;
             f.write_all(jargon.as_bytes())?;
-            // println!("{}", jargon);
+            println!("Encoded");
             Ok(())
         }
-        "-d" => {
-            let words = fs::read_to_string("foobar.txt").expect("Should be able to read from file");
+        "d" | "decode" => {
+            let words = fs::read_to_string(target_path).expect("Should be able to read from file");
             let convert_to_binary = convert_to_bin(words);
             let readable_text = decode(convert_to_binary);
-            let mut file = File::create("decoded.txt")?;
+            let mut file = File::create(destination_path)?;
             file.write_all(readable_text.as_bytes())?;
-            // println!("{}", readable_text);
+            println!("Decoded",);
             Ok(())
         }
         _ => Ok(()),
